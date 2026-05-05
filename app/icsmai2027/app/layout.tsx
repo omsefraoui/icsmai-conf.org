f4671246c2387939fe85e37fcec08023f16d4e90
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2026-05-05 13:20:23 +0000

    test

--- app/icsmai2027/app/layout.tsx
+++ app/icsmai2027/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "./components/layout/header";
-import Footer from "./components/layout/footer";
+import Header from "../components/layout/header";
+import Footer from "../components/layout/footer";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 
