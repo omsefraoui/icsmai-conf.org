c5b3762ce2208a3298be9ba8b605c11a0c5a1e18
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2026-06-08 22:08:38 +0000

    test

--- app/icsmai2027/components/committees.tsx
+++ app/icsmai2027/components/committees.tsx
@@ -197,6 +197,7 @@ export default function Committees() {
       //"H. BOUALI, ESTO, UMP, Oujda, Morocco",
       //"R. BENKADDOUR, ESTO, UMP, Oujda, Morocco",
       "M. JEYAR, ESTO, UMP, Oujda, Morocco",
+      "N. Davanam, Texas, AMD, USA",
       //"M. ABBOUD-ABID ENP, Oran, Algérie",
       //"I. KISSAMI, Mohamed VI Polytechnic University, Morocco",
       "M. FAYAD, Marie and louis Pasteur University, France",
